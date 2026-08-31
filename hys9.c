8ad9401b6638a57df12cee6cdf09d61b3aa5477a
vaibhavsonje1310-beep <vaibhavsonje1310@gmail.com>
2026-09-01 00:13:28 +0530

    Added C assignment programs

---
+++ hys9.c
@@ -0,0 +1,13 @@
+// Program (9) -> Write a C program to demonstrate the use of single - line and multi-line comments.
+
+#include <stdio.h>
+int main()
+{
+    // This is a single line comment....
+
+    /*
+   this vis a multi-line comment.
+   it can span across multiple lines.
+   */
+    return 0;
+}
